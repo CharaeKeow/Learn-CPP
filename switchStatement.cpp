c0e48d22c9fe131c2c00bab376102966602abcf1
CharaeKeow <34004186+CharaeKeow@users.noreply.github.com>
2019-02-20 16:35:57 +0800

    switch statement

---
+++ switchStatement.cpp
@@ -0,0 +1,33 @@
+//Demonstrate the switch using a simple "help" program.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int choice;
+
+	cout << "Help on: \n\n";
+	cout << "1. for\n";
+	cout << "2. if\n";
+	cout << "3. switch\n";
+
+	cout << "Enter choice (1-3): ";
+	cin >> choice;
+	cout << "\n";
+
+	switch (choice) {
+	case 1:
+		cout << "for is C++'s most versatile loop.\n";
+		break;
+	case 2:
+		cout << "if is C++'s conditional branch statement.\n";
+		break;
+	case 3:
+		cout << "switch is C++'s multiway branch statement.\n";
+		break;
+	default:
+		cout << "You must enter a number between 1 and 3.\n";
+	}
+
+	return 0;
+}
