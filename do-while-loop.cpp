5f4cb2d5c9f40072b70a8d0af633392f5e93ea2e
CharaeKeow <34004186+CharaeKeow@users.noreply.github.com>
2019-02-21 20:26:06 +0800

    do-while loop

---
+++ do-while-loop.cpp
@@ -0,0 +1,15 @@
+//Do-while loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+
+	do {
+		cout << "Enter a number (100 to stop): ";
+		cin >> num;
+	} while (num != 100);
+
+	return 0;
+}
